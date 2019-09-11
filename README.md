64cf5cff2e2d4b82bd5dc3ed9493ae439f4e44f1
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-09-10 17:06:32 -0700

    readme

--- README.md
+++ README.md
@@ -21,18 +21,65 @@ $ npm install
 ## Step 3. Start Ganache
 Open the Ganache GUI client that you downloaded and installed. This will start your local blockchain instance.
 
-
-## Step 4. Compile & Deploy Election Smart Contract
+## Step 4. Compile & Deploy Election Smart Contract (Locally)
 `$ truffle migrate --reset`
 You must migrate the election smart contract each time your restart ganache.
 
-## Step 5. Configure Metamask
+## Step 5. Configure Metamask (Locally)
 See free video tutorial for full explanation of these steps:
 - Unlock Metamask
-- Connect metamask to your local Etherum blockchain provided by Ganache.
+- Connect metamask to your local Ganache blockchain.
 - Import an account provided by ganache.
 
 ## Step 6. Run the Front End Application
 `$ npm run dev`
 Visit this URL in your browser: http://localhost:3000
 
+## Step 6. Deploying to SKALE sidechain
+Go to truffle-config.js and replace existing code with the following code:
+
+```s
+require('dotenv').config();
+let HDWalletProvider = require("truffle-hdwallet-provider");
+
+let privateKey = process.env.PRIVATE_KEY;
+
+let rinkeby = process.env.RINKEBY;
+
+let skale = process.env.SKALE_CHAIN;
+
+module.exports = {
+    networks: {
+        ganache: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        rinkeby: {
+            provider: () => new HDWalletProvider(privateKey, rinkeby),
+            gasPrice: 0,
+            network_id: "*"
+        },
+        skale: {
+            provider: () => new HDWalletProvider(privateKey, skale),
+            gasPrice: 0,
+            network_id: "*"
+        }
+    }
+}`
+```
+```
+$ touch .env
+$ cd .env
+```
+enter in your .env file
+```
+MNEMONIC=[your phrase]
+ACCOUNT=[your account]
+PRIVATE_KEY=[your private key]
+SKALE_CHAIN=[your chain endpoint]
+``
+
+```
+truffle deploy --reset --network skale --compile-all
+```
--- truffle-config.js
+++ truffle-config.js
@@ -1,30 +1,44 @@
-require('dotenv').config();
-let HDWalletProvider = require("truffle-hdwallet-provider");
-
-//https://developers.skalelabs.com for SKALE documentation
-//Provide your wallet private key
-let privateKey = process.env.PRIVATE_KEY;
-
-let rinkeby = process.env.RINKEBY;
-
-let skale = process.env.SKALE_CHAIN;
-
 module.exports = {
-    networks: {
-        ganache: {
-            host: "127.0.0.1",
-            port: 7545,
-            network_id: "*"
-        },
-        rinkeby: {
-            provider: () => new HDWalletProvider(privateKey, rinkeby),
-            gasPrice: 0,
-            network_id: "*"
-        },
-        skale: {
-            provider: () => new HDWalletProvider(privateKey, skale),
-            gasPrice: 0,
-            network_id: "*"
-        }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
     }
-}
+  }
+};
+// require('dotenv').config();
+// let HDWalletProvider = require("truffle-hdwallet-provider");
+
+// //https://developers.skalelabs.com for SKALE documentation
+// //Provide your wallet private key
+// let privateKey = process.env.PRIVATE_KEY;
+
+// let rinkeby = process.env.RINKEBY;
+
+// let skale = process.env.SKALE_CHAIN;
+
+// module.exports = {
+//     networks: {
+//         ganache: {
+//             host: "127.0.0.1",
+//             port: 7545,
+//             network_id: "*"
+//         },
+//         rinkeby: {
+//             provider: () => new HDWalletProvider(privateKey, rinkeby),
+//             gasPrice: 0,
+//             network_id: "*"
+//         },
+//         skale: {
+//             provider: () => new HDWalletProvider(privateKey, skale),
+//             gasPrice: 0,
+//             network_id: "*"
+//         }
+//     }
+// }
